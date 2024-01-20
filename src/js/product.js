a5a269c99a7aa294062d73e5c33a282b34fa8fe9
Izak Hearn <izakhearn@gmail.com>
2024-01-20 11:18:46 +0200

    Dynamic Product Cards

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,8 @@
-import { setLocalStorage,getParams } from "./utils.mjs";
+import { getParams } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
 const productId = getParams();
-
-console.log(dataSource.findProductById(productId));
-
 const product = new ProductDetails(productId, dataSource);
 product.init();
